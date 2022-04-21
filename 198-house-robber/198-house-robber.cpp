94c8eff2932e7532a38159e1a033cc1b3dda65f1
Aditi-hande <51283791+Aditi-hande@users.noreply.github.com>
2022-04-21 14:43:56 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (48.08%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,15 +1,18 @@
 class Solution {
+
+    //note that this is top down which is essentially creating a tree but with memoisation/caching
+        
+        int help(vector<int>& num,int n,vector<int>&dp){
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        int n1= num[n]+help(num,n-2,dp);
+        int n2=help(num,n-1,dp);
+        return dp[n]=max(n1,n2);
+        
+    }
 public:
     int rob(vector<int>& nums) {
-        
-        //note that this is bottom up as we are tabulating results and then giving final ans
-        
-        int mx1=0,mx2=0;
-        for(int i=0;i<nums.size();i++){
-            int temp=mx2;
-            mx2=max(mx2,mx1+nums[i]);
-            mx1=temp;
-            cout<<mx1<<mx2<<" ";
-        }
-        return mx2;}
+        vector<int>dp(nums.size()+1,-1);
+        return help(nums,nums.size()-1,dp);
+    }
 };
