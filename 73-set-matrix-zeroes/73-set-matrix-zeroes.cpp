885beea54831a940ce1b7a07b94b879f59af8170
Aditi-hande <51283791+Aditi-hande@users.noreply.github.com>
2022-05-20 11:54:15 +0530

    Time: 32 ms (10.02%), Space: 13.3 MB (18.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,33 +2,38 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        vector<pair<int,int>> zero;
+        //note that improvement to previous is that we dont need multiple array inputs as unique will make sure all in same row or col will be 0 so vector is changed to set
         
+        vector<pair<int,int>> zero;
+        set<int> row_set ;
+    set<int> col_set ;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 
                 if(matrix[i][j]==0) {
-                    auto temp = make_pair(i,j); 
-                    zero.push_back(temp);
+                    row_set.insert(i);
+                    col_set.insert(j);
                 }
                 
             }
         }
         
-        for(auto p:zero){
-            
-            for(int i=0;i<matrix.size();i++){
-                matrix[i][p.second]=0;
-            }
-            
-            for(int j=0;j<matrix[0].size();j++){
-                matrix[p.first][j]=0;
-            }
+       
             
 
-            
-            
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                
+                if (row_set.count(i) || col_set.count(j)) {
+          matrix[i][j] = 0;
         }
+                
+            }
+        }
+            
+            
+        
+ 
         
     }
 };
