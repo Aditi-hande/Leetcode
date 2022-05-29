3edac6cf93bfe35a42ce4433e4ba5c6a15de2e89
Aditi-hande <51283791+Aditi-hande@users.noreply.github.com>
2022-05-29 13:36:04 +0530

    Time: 530 ms (19.76%), Space: 165.1 MB (6.34%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    
-    void helper(int& n,int& k, int curridx, int currlen,vector<int> currans, vector<vector<int>>& ans){
+    vector<vector<int>>ans;
+    void helper(int& n,int& k, int curridx,vector<int> currans){
         
-        if(currlen>k)return;
+        if(currans.size()>k)return;
         
-        if(currlen==k){
+        if(currans.size()==k){
             ans.push_back(currans);
             return;
         }
         
         for(int i=curridx;i<=n;i++){
             currans.push_back(i);
-            currlen++;
-            helper(n,k,i+1,currlen,currans,ans);
+            helper(n,k,i+1,currans);
             currans.pop_back();
-            currlen--;
         }
         
     }
     
     vector<vector<int>> combine(int n, int k) {
         
-        vector<vector<int>>ans;
+        
         vector<int> currans;
         
-        helper(n,k,1,0,currans,ans);
+        helper(n,k,1,currans);
         
         return ans;
                 
