9149e5ebf7dba8697063016b62929adc09110fdd
Aditi Hande <aditi321hande@gmail.com>
2024-01-21 14:39:14 -0800

    Time: 279 ms (97.68%), Space: 32 MB (95.58%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,11 +5,51 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        end=head
-        s="".join(str(end.val))
-        while end.next:
-            end=end.next
-            s=s+str(end.val)
+        
+        # to optimize O(1) space, reverse second half of list and then compare
+        #can find mid using fast slow pointer
+        
+        fast=slow=head
+        
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        #slow will be at mid
+        
+        #reverse from mid to end
+        prev=None
+        cur=slow
+        while(cur):
+            temp=cur.next
+            cur.next=prev
+            prev=cur
+            cur=temp
+        
+        #compare
+        
+        lo=head
+        hi=prev
+        
+        while hi:
+            if lo.val!=hi.val:
+                return False
+            lo=lo.next
+            hi=hi.next
+        return True
+        
+        
+            
+        
+
+        
+        
+        
+#         end=head
+#         s="".join(str(end.val))
+#         while end.next:
+#             end=end.next
+#             s=s+str(end.val)
 
             
-        return s==s[::-1]
+#         return s==s[::-1]
+#since space is O(n)
