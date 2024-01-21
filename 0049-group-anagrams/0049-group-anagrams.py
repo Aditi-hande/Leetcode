420844385dbd0979a5a8d4ddf1195dbd9265b744
Aditi Hande <aditi321hande@gmail.com>
2024-01-21 06:05:11 -0800

    Time: 276 ms (5.02%), Space: 21.3 MB (37.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        map=collections.defaultdict(list)
         
-        for i in range(len(strs)):
-            temp=''.join(sorted(strs[i]))
-            map[temp].append(strs[i])
+        
+#         map=collections.defaultdict(list)
+        
+#         for i in range(len(strs)):
+#             temp=str(Counter(strs[i]))
+#             # temp=''.join(sorted(strs[i]))
+#             map[temp].append(strs[i])
             
-        return list(map.values())
-        
+#         for str1 in strs:
+#             print(str1,Counter(str1),str(Counter(str1)))
+            
+#         return list(map.values())
+
+        d = collections.defaultdict(list)
+        y = Counter('abcdefghijklmnopqrstuvwxyz')
+        for s in strs:
+            z = Counter(y) + Counter(s)
+            # print(z)
+            key = ''.join(str(z.values()))
+            # print(type(key))
+            d[key].append(s)
+
+        return d.values()
