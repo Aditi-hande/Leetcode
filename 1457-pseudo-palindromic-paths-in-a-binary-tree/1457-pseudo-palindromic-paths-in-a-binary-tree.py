72920d7bade4cee12230b783987f6715f507f3c1
Aditi Hande <aditi321hande@gmail.com>
2024-01-24 09:03:30 -0800

    Time: 329 ms (98.67%), Space: 43.4 MB (98.67%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        count = 0
+        
+        stack = [(root, 0) ]
+        while stack:
+            node, path = stack.pop()
+            if node is not None:
+                # compute occurences of each digit 
+                # in the corresponding register
+                path = path ^ (1 << node.val)
+                # if it's a leaf, check if the path is pseudo-palindromic
+                if node.left is None and node.right is None:
+                    # check if at most one digit has an odd frequency
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:
+                    stack.append((node.right, path))
+                    stack.append((node.left, path))
+        
+        return count
+
+        #Left shift operator is used to define the bit, and XOR operator - to compute the digit frequency.
+        # we can use a bit trick (n & (n - 1)) to remove the rightmost set bit.
+        #if the result is 0, that means we have at most one digit that has an odd frequncy 
+
+
+
+        
